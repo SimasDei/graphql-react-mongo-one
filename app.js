bda97457e39c06a902bb099cda2e51eed6e664b3
SimasDei <simondeiman@gmail.com>
2019-01-26 11:58:14 +0200

    add bcryptjs to hash passwords, check for unique email on registration

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const Event = require('./models/event');
+const User = require('./models/user');
 
 // Parse http requests
 app.use(bodyParser.json());
@@ -22,6 +24,12 @@ app.use(
       price: Float!
       date: String!
     }
+
+    type User {
+      _id: ID!
+      email: String!
+      password: String
+    }
     
     input EventInput {
       title: String!
@@ -30,12 +38,18 @@ app.use(
       date: String!
     }
     
+    input UserInput {
+      email: String!
+      password: String!
+    }
+
     type RootQuery {
       events: [Event!]!
     }
     
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
   
     schema {
@@ -73,6 +87,28 @@ app.use(
             throw err;
           });
         return event;
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error('Email already in use.');
+            }
+            return bcrypt.hash(args.userInput.password, 10);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null, _id: result.id };
+          })
+          .catch(err => {
+            throw err;
+          });
       }
     },
     graphiql: true
