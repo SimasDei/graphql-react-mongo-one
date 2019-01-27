e5c69dd50292a31fa3bce7ed07033ccdf0b6803a
SimasDei <simondeiman@gmail.com>
2019-01-27 12:35:12 +0200

    add top navbar + styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,23 @@ import './App.css';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './components/navigation/navigation';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
       </BrowserRouter>
     );
   }
---
+++ frontend/src/components/navigation/navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './navigation.css';
+
+const mainNavigation = props => (
+  <header className="main-navigation">
+    <div className="main-navigation__logo">
+      <h1>Eventador</h1>
+    </div>
+    <nav className="main-navigation__items">
+      <ul>
+        <li>
+          <NavLink to={'/auth'}>Login</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/events'}>Events</NavLink>
+        </li>
+        <li>
+          <NavLink to={'/bookings'}>Bookings</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
+
+export default mainNavigation;
