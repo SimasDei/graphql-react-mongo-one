0692f0425a528b4cf14ee1cc01fcb571968f0a48
SimasDei <simondeiman@gmail.com>
2019-01-28 14:03:59 +0200

    use react built in context, to share user authentication state with different components and render them accordingly

--- frontend/src/components/navigation/navigation.js
+++ frontend/src/components/navigation/navigation.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import AuthContext from '../../context/auth-context';
+
 import './navigation.css';
 
 const mainNavigation = props => (
-  <header className="main-navigation">
-    <div className="main-navigation__logo">
-      <h1>Eventador</h1>
-    </div>
-    <nav className="main-navigation__items">
-      <ul>
-        <li>
-          <NavLink to={'/auth'}>Login</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/events'}>Events</NavLink>
-        </li>
-        <li>
-          <NavLink to={'/bookings'}>Bookings</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <AuthContext.Consumer>
+    {context => {
+      return (
+        <header className="main-navigation">
+          <div className="main-navigation__logo">
+            <h1>Eventador</h1>
+          </div>
+          <nav className="main-navigation__items">
+            <ul>
+              {!context.token && (
+                <li>
+                  <NavLink to={'/auth'}>Login</NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to={'/events'}>Events</NavLink>
+              </li>
+              {context.token && (
+                <li>
+                  <NavLink to={'/bookings'}>Bookings</NavLink>
+                </li>
+              )}
+            </ul>
+          </nav>
+        </header>
+      );
+    }}
+  </AuthContext.Consumer>
 );
 
 export default mainNavigation;
